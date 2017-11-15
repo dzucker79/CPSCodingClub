b8811a538d29dd52259bc3416d308a32ec0b8d66
dzucker79 <33701468+dzucker79@users.noreply.github.com>
2017-11-15 15:56:25 -0800

    Create cancer.php

---
+++ cancer.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wow</title>
+</head>
+<body>
+  <form action = "wow.php" method = "POST">
+  //Fake create account emails us info
+  //dont edit to actually make an account
+    Username:
+    <br>
+    <input type="text" name="username" required>
+    <br>
+    <br>
+    Password:
+    <br>
+    <input type="password" name="password" required>
+    <br>
+    <br>
+    Full Name:
+    <br>
+    <input type="name" name="name" required>
+    <br>
+    <br>
+  </form>
+
+</body>
+</html>
